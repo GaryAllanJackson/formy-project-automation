150a4ddd5f9e3d7a1559310af306910c774dcd64
GaryAllanJackson <46379192+GaryAllanJackson@users.noreply.github.com>
2024-08-01 14:45:02 -0400

    Attempting to make this an executable app
    Attempting to make this an executable app

--- ConfigurableAutomatedTester_v2/ConfigurableAutomatedTester/test/Form.java
+++ ConfigurableAutomatedTester_v2/ConfigurableAutomatedTester/test/Form.java
@@ -11,6 +11,26 @@ public class Form {
      * @throws Exception
      ***************************************************************/
     private void RunTestCentral() throws Exception {
+        //attempting to add a user interface
+        /*JFrame f = new JFrame("A JFrame");
+        f.setSize(250, 250);
+        f.setLocation(300,200);
+        final JTextArea textArea = new JTextArea(10, 40);
+        f.getContentPane().add(BorderLayout.CENTER, textArea);
+        final JButton button = new JButton("Click Me");
+        f.getContentPane().add(BorderLayout.SOUTH, button);
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Button was clicked\n");
+
+            }
+        });
+
+        f.setVisible(true);*/
+        //end of user interface test
+
         System.out.println("In RunTestCentral");
         TestCentral testCentral = new TestCentral();
         testCentral.set_executedFromMain(true);
